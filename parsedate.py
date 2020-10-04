2c8ce45560a25454ea4e0ef66b1501936038306f
Dimuth Kurukula <dimuthkurukula@gmail.com>
2020-10-03 20:18:41 -0400

    init parsing dates

---
+++ parsedate.py
@@ -0,0 +1,15 @@
+"""
+Parse date of downloaded radar gifs
+"""
+
+import re
+
+def parseurl():
+    pass
+
+def main():
+    pass
+
+if __nam__ == '__main__':
+    main()
+
---
+++ test_parsedate.py
@@ -0,0 +1,5 @@
+import parsedate
+
+
+def test_parsedate():
+    parsedate.parseurl() == '2020-01-01 01:00'
